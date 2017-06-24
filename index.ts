75f690cb69578c76b969d74a96d09ac16def46f1
Igor N. Dultsev <yhaskell@live.com>
2017-06-24 18:13:37 +0700

    add reading from file

--- index.ts
+++ index.ts
@@ -1,23 +1,54 @@
+#!/usr/bin/env node
+
 import { Computer } from './computer'
 import * as readline from 'readline'
+import * as fs from 'fs'
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
 
-const computer = new Computer()
+function REPL() {
+    const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+    });
 
-function lineRead(line: string) {
-    if (line != "") {
-        try {
-            const result = computer.compute(line)
-            console.log(result.toString())
-        } catch (err) {
-            console.log(err.message)
+    const computer = new Computer()
+
+    function lineRead(line: string) {
+        if (line != "") {
+            try {
+                const result = computer.compute(line)
+                console.log(result.toString())
+            } catch (err) {
+                console.log(err.message)
+            }
         }
+        rl.question("> ", lineRead)
     }
+
     rl.question("> ", lineRead)
 }
 
-rl.question("> ", lineRead)
+function fromFile(filename: string) {
+    const computer = new Computer();
+    let lines: string[]
+    try {
+        lines = fs.readFileSync(filename, "utf-8").split("\n")
+    } catch (err) {
+        console.error(`cannot open file ${filename} for reading.`)
+        return 
+    }
+    
+    for (const line of lines) 
+        if (line != "") computer.compute(line)
+    
+    console.log(computer.lastComp.toString())
+}
+
+if (process.argv.length == 3) {
+    fromFile(process.argv[process.argv.length - 1])
+} else if (process.argv.length == 2) {
+    REPL()
+} else {
+    console.error("Usage: " + process.argv0 + " " + process.argv[1] + " [FILE]")
+    process.exit(1)
+}
